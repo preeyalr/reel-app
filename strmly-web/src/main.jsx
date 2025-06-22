23308ced82205c4d8d755619b47db97a57695fbe
Preeyal Rathod <138372764+preeyalr@users.noreply.github.com>
2025-06-22 08:54:19 +0000

    change

--- strmly-web/src/main.jsx
+++ strmly-web/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Login from './screens/Login.jsx'
+import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    
-    <Login />
+   <App />
   </StrictMode>,
 )
